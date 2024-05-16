cbff8e3f303bed68578a5d1ecbecd895eb525c53
AHReccese <amirhssin6rst@gmail.com>
2024-05-16 15:25:34 +0330

    serve and live diagrams with streamlit

---
+++ stream.py
@@ -0,0 +1,30 @@
+import lead as ld
+import streamlit as st
+
+school_name = st.selectbox(
+    "School Name:",
+    ld.dws_names,
+    # default option
+    index=ld.dws_names.index('R243 LAMBTON KENT COMP S (5473)'))
+
+years = st.multiselect(
+    "Year:",
+    ld.years,
+    # default option
+    ld.years[0:2]
+    )
+
+categories = st.multiselect(
+    "Category:",
+    ld.categories,
+    # default option
+    ld.categories[0:2]
+    )
+
+pie = ld.get_pie(school_name, categories, years)
+histogram = ld.get_histogram(school_name, categories, years)
+line = ld.get_line(school_name, categories, years)
+
+st.altair_chart(pie, use_container_width= True)
+st.altair_chart(histogram, use_container_width= True)
+st.altair_chart(line, use_container_width= True)
